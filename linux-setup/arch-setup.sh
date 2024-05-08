39069dd7d617201658aed9c02aa03366c851520a
rezzect <rezzect6697@gmail.com>
2024-05-08 15:23:11 -0700

    a

--- linux-setup/arch-setup.sh
+++ linux-setup/arch-setup.sh
@@ -83,10 +83,10 @@ installOhMyPosh() {
 }
 
 linkConfig() {
-    ln -svf ~/.rezz-dotfiles/zsh/.zshrc ~/.zshrc
-    ln -svf ~/.rezz-dotfiles/omp-themes/rezztheme-edit.omp.json ~/.omp-themes/
-    ln -svf ~/.rezz-dotfiles/kitty/kitty.conf ~/.config/kitty/
-    ln -svf ~/.rezz-dotfiles/ssh/authorized_keys ~/.ssh/
+    ln -s ~/.rezz-dotfiles/zsh/.zshrc ~/.zshrc
+    ln -s ~/.rezz-dotfiles/omp-themes/rezztheme-edit.omp.json ~/.omp-themes/
+    ln -s ~/.rezz-dotfiles/kitty/kitty.conf ~/.config/kitty/
+    ln -s ~/.rezz-dotfiles/ssh/authorized_keys ~/.ssh/
 }
 
 checkEnv
